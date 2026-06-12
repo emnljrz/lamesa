8d81ff92c367a5c03c96f14f16b390bf4804a4d7
emnljrz <emnljrz@outlook.com>
2026-06-12 16:06:09 +0800

    fix: CI add vitest coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
     coverage: {
       provider: 'v8',
-      reporter: ['text', 'json', 'lcov'], // 'lcov' is what Codecov needs!
+      reporter: ['text', 'json', 'lcov'], // Crucial for Codecov
+      exclude: [
+        '**/node_modules/**',
+        '**/dist/**',
+        'packages/angular/**',
+        'vitest.config.ts',
+      ],
     },
   },
-});
+})
